6c9825ce0ca7f981b9f688e7409aaee08a675a5c
José Alonso <mail@josealonso.dev>
2025-05-06 02:17:11 -0600

    feat: implement user registration logic in AuthService with error handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,46 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from 'generated/prisma';
+import { RegisterUserDto } from './dto';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('auth-ms');
+
   onModuleInit() {
     this.$connect();
     this.logger.log('MongoDB connected');
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    const { name, email, password } = registerUserDto;
+
+    try {
+      const user = await this.user.findUnique({
+        where: { email },
+      });
+
+      if (user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User already exists',
+        });
+      }
+
+      const newUser = await this.user.create({
+        data: {
+          name: name,
+          email: email,
+          password: password, // TODO: encriptar la clave
+        },
+      });
+
+      return { user: newUser, token: 'ABC' };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
   }
 }
