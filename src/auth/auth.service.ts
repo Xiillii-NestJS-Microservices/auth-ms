e2953307b611b63d2a075ce22a358f41201a8503
José Alonso <mail@josealonso.dev>
2025-05-06 20:14:37 -0600

    feat: implement login user logic with password validation in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from 'generated/prisma';
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
@@ -37,6 +37,41 @@ export class AuthService extends PrismaClient implements OnModuleInit {
       });
 
       const { password: __, ...restDataUser } = newUser;
+
+      return { user: restDataUser, token: 'ABC' };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    try {
+      const user = await this.user.findUnique({
+        where: { email },
+      });
+
+      if (!user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid',
+        });
+      }
+
+      const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+      if (!isPasswordValid) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid',
+        });
+      }
+
+      const { password: ___, ...restDataUser } = user;
 
       return { user: restDataUser, token: 'ABC' };
     } catch (error) {
